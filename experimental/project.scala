e38c944135870152e950863c72c63e85ce042a72
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2023-09-11 14:16:53 +0200

    next batch of friday evening's typos

--- experimental/project.scala
+++ experimental/project.scala
@@ -1,4 +1,5 @@
 //> using scala "3.3.0"
-//> using dep "org.virtuslab::besom-core:4.13.2-beta.0.0.1"
-//> using dep "org.virtuslab::besom-kubernetes:4.1.1-beta.0.0.1"
+
+//> using dep "org.virtuslab::besom-core:0.0.1-beta"
+//> using dep "org.virtuslab::besom-kubernetes:4.1.1-SNAPSHOT.0.0.1"
 //> using dep "io.github.iltotore::iron:2.1.0"
--- template/kubernetes/project.scala
+++ template/kubernetes/project.scala
@@ -1,3 +1,3 @@
 //> using scala "3.3.0"
-//> using dep "org.virtuslab::besom-core:4.13.2-beta.0.0.1"
+//> using dep "org.virtuslab::besom-core:0.0.1-beta"
 //> using dep "org.virtuslab::besom-kubernetes:4.1.1-beta.0.0.1"
