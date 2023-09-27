7c32539f4700b10652a0d80b144f36a1e5485297
Paweł Prażak <pprazak@virtuslab.com>
2023-09-27 11:31:38 +0200

    Add aws template

---
+++ templates/aws/project.scala
@@ -0,0 +1,4 @@
+//> using scala "3.3.0"
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.0.1-beta"
+//> using dep "org.virtuslab::besom-core:0.0.1-beta"
+//> using dep "org.virtuslab::besom-aws:5.42.0-beta.0.0.1"
