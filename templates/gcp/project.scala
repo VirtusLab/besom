9284cca826c55c7d212a5192d27d4a3f0eed4d30
Paweł Prażak <pprazak@virtuslab.com>
2023-10-04 15:54:22 +0200

    Add gcp template

---
+++ templates/gcp/project.scala
@@ -0,0 +1,4 @@
+//> using scala "3.3.1"
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.1.0"
+//> using dep "org.virtuslab::besom-core:0.1.0"
+//> using dep "org.virtuslab::besom-gcp:6.66.0-core.0.1.0"
