2fb5f7a09f2b4f46b33d3e4eed9710f0f7b2facd
Paweł Prażak <pprazak@virtuslab.com>
2023-10-05 14:37:32 +0200

    Add kubernetes-nginx example

---
+++ examples/kubernetes-nginx/project.scala
@@ -0,0 +1,5 @@
+//> using scala "3.3.1"
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.1.0"
+//> using dep "org.virtuslab::besom-core:0.1.0"
+//> using dep "org.virtuslab::besom-kubernetes:4.3.0-core.0.1.0"
+//> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
