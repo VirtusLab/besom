b97fb2a42094c691e1b5fcd75f23a8ddc8c3153b
Paweł Prażak <pprazak@virtuslab.com>
2023-10-06 11:30:14 +0200

    Add docker-multi-container-app example

---
+++ examples/docker-multi-container-app/app/project.scala
@@ -0,0 +1,9 @@
+//> using scala 3.3.1
+//> using options -java-output-version:17 -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
+
+//> using dep com.softwaremill.sttp.tapir::tapir-jdkhttp-server:1.9.3
+//> using dep redis.clients:jedis:5.1.0
+
+//> using publish.organization org.virtuslab.besom.example
+//> using publish.name app
+//> using publish.version 0.0.1
---
+++ examples/docker-multi-container-app/infra/Main.scala
@@ -0,0 +1,97 @@
+import besom.*
+import besom.api.docker
+import docker.inputs._
+
+@main def main = Pulumi.run {
+  // Set defaults for redis
+  val redisPort = 6379
+  val redisHost = "redisdb"
+
+  // Create docker network
+  val network = docker.Network(
+    "network",
+    docker.NetworkArgs(
+      name = "services"
+    )
+  )
+
+  // Get redis image
+  val redisImage = docker.RemoteImage(
+    "redisImage",
+    docker.RemoteImageArgs(
+      name = "redis:6.2",
+      keepLocally = true
+    )
+  )
+
+  // Run redis image
+  val redisContainer = docker.Container(
+    "redisContainer",
+    docker.ContainerArgs(
+      image = redisImage.imageId,
+      ports = List(
+        ContainerPortArgs(
+          internal = redisPort,
+          external = redisPort
+        )
+      ),
+      networksAdvanced = List(
+        ContainerNetworksAdvancedArgs(
+          name = network.name,
+          aliases = Output(redisHost).map(List(_))
+        )
+      )
+    )
+  )
+
+  // Build image from local app directory
+/*  val appImage = docker.Image(
+    "appImage",
+    docker.ImageArgs(
+      build = DockerBuildArgs(
+        context = "../app"
+      ),
+      imageName = "app",
+      skipPush = true
+    )
+  )*/
+
+  // Set external port for app url
+  val appPort = 3000
+
+  // Run container from local app image
+  val appContainer = docker.Container(
+    "appContainer",
+    docker.ContainerArgs(
+      image = "app",
+      ports = List(
+        ContainerPortArgs(
+          internal = appPort,
+          external = appPort
+        )
+      ),
+      envs = List(
+        p"APP_PORT=${appPort}",
+        p"REDIS_HOST=${redisHost}",
+        p"REDIS_PORT=${redisPort}"
+      ),
+      networksAdvanced = List(
+        ContainerNetworksAdvancedArgs(
+          name = network.name
+        )
+      )
+    ),
+    besom.CustomResourceOptions(
+      dependsOn = redisContainer.map(List(_))
+    )
+  )
+
+  for
+    _ <- redisImage
+    _ <- redisContainer
+    _ <- appContainer
+  yield exports(
+    redis = p"redis://${redisHost}:${redisPort}",
+    url = p"http://localhost:${appPort}"
+  )
+}
