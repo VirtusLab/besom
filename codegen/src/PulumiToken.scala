daddff16760d911d4236139cf5e92be6b2ca8955
Paweł Prażak <pprazak@virtuslab.com>
2023-11-20 14:36:00 +0100

    Fix provider coordinates vs token confusion
    Fixes #304

--- codegen/src/PulumiToken.scala
+++ codegen/src/PulumiToken.scala
@@ -3,12 +3,15 @@ package besom.codegen
 import scala.util.matching.Regex
 
 case class PulumiToken private (provider: String, module: String, name: String) {
-  def asString: String = s"${provider}:${module}:${name}"
+  def asString: String  = s"${provider}:${module}:${name}"
+  def uniformed: String = asString.toLowerCase
 }
 
 object PulumiToken {
-  private val tokenFmt: String    = "(.*):(.*)?:(.*)" // provider:module:name
-  private val tokenPattern: Regex = ("^" + tokenFmt + "$").r
+  private val tokenFmtShort: String = "([^:]*):([^:]*)" // provider:name
+  private val tokenPatternShort: Regex = ("^" + tokenFmtShort + "$").r
+  private val tokenFmt: String      = "([^:]*):([^:]*)?:([^:]*)" // provider:module:name
+  private val tokenPattern: Regex   = ("^" + tokenFmt + "$").r
 
   private def enforceNonEmptyModule(module: String): String =
     module match {
@@ -17,12 +20,16 @@ object PulumiToken {
     }
 
   def apply(token: String): PulumiToken = token match {
-    case tokenPattern("pulumi", "providers", providerName) =>
-      new PulumiToken(providerName, Utils.indexModuleName, Utils.providerTypeName)
     case tokenPattern(provider, module, name) =>
       new PulumiToken(
         provider = provider,
         module = enforceNonEmptyModule(module),
+        name = name
+      )
+    case tokenPatternShort(provider, name) =>
+      new PulumiToken(
+        provider = provider,
+        module = Utils.indexModuleName,
         name = name
       )
     case _ => throw TypeMapperError(s"Cannot parse Pulumi token: $token, tokenPattern: $tokenPattern")
