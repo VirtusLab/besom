b30e5154e5d4ca33fbe3677f2989765750b2834e
Paweł Prażak <pprazak@virtuslab.com>
2023-12-01 11:52:01 +0100

    Update logger integration test

--- integration-tests/resources/logger-example/Main.scala
+++ integration-tests/resources/logger-example/Main.scala
@@ -3,5 +3,6 @@ import besom.*
 @main def run = Pulumi.run {
   for
     _ <- log.warn("Nothing here yet. It's waiting for you!")
+    _ <- p"Interpolated ${Output("value")}".flatMap(log.info(_))
   yield exports()
 }
