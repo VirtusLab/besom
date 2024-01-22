682b0269d158ae11fc79c9af5dfcc2534562a25e
Paweł Prażak <pprazak@virtuslab.com>
2024-01-22 11:20:58 +0100

    Update all versions in examples

--- examples/aws-secrets-manager/project.scala
+++ examples/aws-secrets-manager/project.scala
@@ -1,5 +1,5 @@
 //> using scala "3.3.1"
-//> using plugin "org.virtuslab::besom-compiler-plugin:0.1.0"
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.1.1-SNAPSHOT"
 //> using dep "org.virtuslab::besom-core:0.1.1-SNAPSHOT"
-//> using dep "org.virtuslab::besom-aws:6.2.1-core.0.1.1-SNAPSHOT"
+//> using dep "org.virtuslab::besom-aws:6.18.1-core.0.1.1-SNAPSHOT"
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
--- templates/default/project.scala
+++ templates/default/project.scala
@@ -1,4 +1,4 @@
 //> using scala "3.3.1"
-//> using plugin "org.virtuslab::besom-compiler-plugin:0.1.0"
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.1.1-SNAPSHOT"
 //> using dep "org.virtuslab::besom-core:0.1.1-SNAPSHOT"
-//> using dep "org.virtuslab::besom-random:4.14.0-core.0.1.1-SNAPSHOT"
+//> using dep "org.virtuslab::besom-random:4.15.1-core.0.1.1-SNAPSHOT"
