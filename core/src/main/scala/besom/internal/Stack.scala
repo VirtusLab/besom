f826c2315f44eaa39031f60cc7c2b0e5b791b058
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-01-25 11:23:19 +0100

    add Scaladoc

--- core/src/main/scala/besom/internal/Stack.scala
+++ core/src/main/scala/besom/internal/Stack.scala
@@ -2,6 +2,15 @@ package besom.internal
 
 import com.google.protobuf.struct.Struct
 
+/** The Stack is the final result of a Pulumi program. It contains the exports and dependencies of the program. * Exports are the values
+  * that are exposed to the Pulumi runtime and available to other stacks via StackReference * Dependencies are the values that have to be
+  * evaluated (and thus created) for the Stack to be created.
+  *
+  * The Stack is created in user's code using [[StackFactory]] and not directly to offer a nicer API.
+  *
+  * @param _exports
+  * @param dependsOn
+  */
 case class Stack private[besom] (private val _exports: Exports, private val dependsOn: Vector[Output[?]]):
   private[besom] def evaluateDependencies(using Context): Result[Unit] =
     Output.sequence(dependsOn).getData.void
--- core/src/main/scala/besom/internal/StackFactory.scala
+++ core/src/main/scala/besom/internal/StackFactory.scala
@@ -1,5 +1,17 @@
 package besom.internal
 
+/** The Stack is the final result of a Pulumi program. It contains the exports and dependencies of the program. * Exports are the values
+  * that are exposed to the Pulumi runtime and available to other stacks via StackReference * Dependencies are the values that have to be
+  * evaluated (and thus created) for the Stack to be created
+  *
+  * There are three ways to create a Stack in user's code:
+  *
+  * * Stack(a, b) - creates a stack with dependencies a and b
+  *
+  * * Stack.exports(a = x, b = y) - creates a stack with exports a and b
+  *
+  * * Stack(a, b).exports(c = x, d = y) - creates a stack with dependencies a and b and exports c and d
+  */
 trait StackFactory:
   val exports: EmptyExport.type = EmptyExport
 
