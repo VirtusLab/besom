001da6d6318cfbfa0aabe577f3909b8c07c51eb5
Paweł Prażak <pprazak@virtuslab.com>
2024-01-26 09:38:55 +0100

    Hotfix example versions

--- examples/aws-webserver/project.scala
+++ examples/aws-webserver/project.scala
@@ -2,5 +2,5 @@
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
 //> using plugin org.virtuslab::besom-compiler-plugin:0.1.1-SNAPSHOT
 //> using dep org.virtuslab::besom-core:0.1.1-SNAPSHOT
-//> using dep org.virtuslab::besom-aws:6.18.1-core.0.1.1-SNAPSHOT
+//> using dep org.virtuslab::besom-aws:6.18.2-core.0.1.1-SNAPSHOT
 //> using dep org.virtuslab::besom-tls:5.0.0-core.0.1.1-SNAPSHOT
