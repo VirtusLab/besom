c411e527a599749e3a00f542ec8b0581511d5060
Paweł Prażak <pprazak@virtuslab.com>
2024-02-01 14:38:10 +0100

    Update dependencies

--- integration-tests/resources/cats-purrl-example/project.scala
+++ integration-tests/resources/cats-purrl-example/project.scala
@@ -3,4 +3,4 @@
 //> using plugin org.virtuslab::besom-compiler-plugin:0.1.1-SNAPSHOT
 //> using dep org.virtuslab::besom-core:0.1.1-SNAPSHOT
 //> using dep org.virtuslab::besom-cats:0.1.1-SNAPSHOT
-//> using dep org.virtuslab::besom-purrl:0.5.0-core.0.1.1-SNAPSHOT
+//> using dep "org.virtuslab::besom-purrl:0.5.0-core.0.1-SNAPSHOT"
