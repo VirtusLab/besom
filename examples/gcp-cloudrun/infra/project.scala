c8dd34466b2b34c149baae4bad5eab98f8cdd14c
Paweł Prażak <pprazak@virtuslab.com>
2024-02-10 20:42:20 +0100

    Update providers to latest

--- examples/gcp-cloudrun/infra/project.scala
+++ examples/gcp-cloudrun/infra/project.scala
@@ -2,7 +2,7 @@
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
 //> using plugin "org.virtuslab::besom-compiler-plugin:0.2.1-SNAPSHOT"
 //> using dep "org.virtuslab::besom-core:0.2.1-SNAPSHOT"
-//> using dep "org.virtuslab::besom-gcp:7.7.1-core.0.2-SNAPSHOT"
+//> using dep "org.virtuslab::besom-gcp:7.8.0-core.0.2-SNAPSHOT"
 //> using dep "org.virtuslab::besom-docker:4.5.1-core.0.2-SNAPSHOT"
 
 //> using repository sonatype:snapshots
