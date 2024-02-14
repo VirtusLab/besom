0236ce6cf0b61f6e450ad43822db28901eda62bd
Paweł Prażak <pprazak@virtuslab.com>
2024-02-14 15:41:55 +0100

    Update providers in examples and templates

--- templates/aws/project.scala
+++ templates/aws/project.scala
@@ -2,6 +2,6 @@
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
 //> using plugin "org.virtuslab::besom-compiler-plugin:0.2.1-SNAPSHOT"
 //> using dep "org.virtuslab::besom-core:0.2.1-SNAPSHOT"
-//> using dep "org.virtuslab::besom-aws:6.20.1-core.0.2-SNAPSHOT"
+//> using dep "org.virtuslab::besom-aws:6.22.0-core.0.2-SNAPSHOT"
 
 //> using repository sonatype:snapshots
