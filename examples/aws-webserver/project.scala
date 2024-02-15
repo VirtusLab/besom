dec36258b63f2822483281a0c89d1502b81f428c
Paweł Prażak <pprazak@virtuslab.com>
2024-02-15 13:37:58 +0100

    Update provider versions to latest

--- examples/aws-webserver/project.scala
+++ examples/aws-webserver/project.scala
@@ -2,7 +2,7 @@
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
 //> using plugin org.virtuslab::besom-compiler-plugin:0.2.1
 //> using dep org.virtuslab::besom-core:0.2.1
-//> using dep org.virtuslab::besom-aws:6.22.0-core.0.2
+//> using dep org.virtuslab::besom-aws:6.22.1-core.0.2
 //> using dep org.virtuslab::besom-tls:5.0.0-core.0.2
 
 //> using repository sonatype:snapshots
