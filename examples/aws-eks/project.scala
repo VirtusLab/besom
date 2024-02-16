52dde92cf5c5cc3c3fdc113c57a1335e22562b8b
Paweł Prażak <pprazak@virtuslab.com>
2024-02-16 12:06:32 +0100

    Update version to 0.2.2-SNAPSHOT

--- examples/aws-eks/project.scala
+++ examples/aws-eks/project.scala
@@ -1,6 +1,7 @@
 //> using scala "3.3.1"
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
-//> using plugin "org.virtuslab::besom-compiler-plugin:0.2.1"
-//> using dep "org.virtuslab::besom-core:0.2.1"
-//> using dep "org.virtuslab::besom-eks:2.2.1-core.0.2"
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.2.2-SNAPSHOT"
+//> using dep "org.virtuslab::besom-core:0.2.2-SNAPSHOT"
+//> using dep "org.virtuslab::besom-eks:2.2.1-core.0.2-SNAPSHOT"
 
+//> using repository sonatype:snapshots
