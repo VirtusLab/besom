271502c1dc035841dd2017ecce66c59a782f072b
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-02-26 10:18:52 +0100

    small corrections for gcp cloudrun example

--- examples/gcp-cloudrun/app/project.scala
+++ examples/gcp-cloudrun/app/project.scala
@@ -1,7 +1,8 @@
 //> using scala 3.3.1
-//> using options -java-output-version:17 -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
+//> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
+//> using jvm "graalvm-java21:21.0.2"
 
-//> using dep com.softwaremill.sttp.tapir::tapir-jdkhttp-server:1.9.9
+//> using dep com.softwaremill.sttp.tapir::tapir-jdkhttp-server:1.9.10
 
 //> using publish.organization org.virtuslab.besom.example
 //> using publish.name app
