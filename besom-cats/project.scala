bceafff214b876f827da4d394c426f376991cfaf
Paweł Prażak <pprazak@virtuslab.com>
2024-02-28 09:29:54 +0100

    Bump to 0.2.3-SNAPSHOT

--- besom-cats/project.scala
+++ besom-cats/project.scala
@@ -2,7 +2,7 @@
 //> using options "-java-output-version:11" "-encoding:utf-8", "-Ykind-projector:underscores"
 //> using options "-deprecation", "-feature"
 
-//> using dep "org.virtuslab::besom-core:0.2.2"
+//> using dep "org.virtuslab::besom-core:0.2.3-SNAPSHOT"
 //> using dep "org.typelevel::cats-effect:3.3.14"
 //> using test.dep "org.scalameta::munit:1.0.0-M10"
 
@@ -16,3 +16,4 @@
 //> using publish.developer "lbialy|Łukasz Biały|https://github.com/lbialy"
 //> using publish.developer "prolativ|Michał Pałka|https://github.com/prolativ"
 //> using publish.developer "KacperFKorban|Kacper Korban|https://github.com/KacperFKorban"
+//> using repository sonatype:snapshots
--- integration-tests/resources/executors/scala-cli/project.scala
+++ integration-tests/resources/executors/scala-cli/project.scala
@@ -1,7 +1,8 @@
 //> using scala 3.3.1
 //> using options -java-output-version:11
-//> using plugin "org.virtuslab::besom-compiler-plugin:0.2.2"
-//> using dep "org.virtuslab::besom-core:0.2.2"
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.2.3-SNAPSHOT"
+//> using dep "org.virtuslab::besom-core:0.2.3-SNAPSHOT"
 //> using dep "org.virtuslab::besom-fake-standard-resource:1.2.3-TEST"
 //> using dep "org.virtuslab::besom-fake-external-resource:2.3.4-TEST"
 
+//> using repository sonatype:snapshots
