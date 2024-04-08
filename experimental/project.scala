0d9fe50164b398f201c10dafe31a0bfdaccd8f41
Paweł Prażak <pawelprazak@users.noreply.github.com>
2024-04-08 15:42:55 +0200

    Fix liftoff example (#442)

--- experimental/project.scala
+++ experimental/project.scala
@@ -1,7 +1,7 @@
-//> using scala "3.3.3"
+//> using scala "3.3.1"
 //> using plugin "org.virtuslab::besom-compiler-plugin:0.2.3-SNAPSHOT"
 //> using dep "org.virtuslab::besom-core:0.2.3-SNAPSHOT"
-//> using dep "org.virtuslab::besom-kubernetes:4.8.0-core.0.2"
+//> using dep "org.virtuslab::besom-kubernetes:4.9.1-core.0.2-SNAPSHOT"
 //> using dep "io.github.iltotore::iron:2.5.0"
 
 //> using repository sonatype:snapshots
