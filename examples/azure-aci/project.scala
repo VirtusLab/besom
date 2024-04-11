95e5a64481e999cc3c62b51b4a7528788cda9460
kpoliwka <kpoliwka@virtuslab.com>
2024-04-11 13:23:01 +0200

    add azure ACI example

---
+++ examples/azure-aci/project.scala
@@ -0,0 +1,8 @@
+//> using scala "3.3.1"
+//> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.2.3-SNAPSHOT"
+
+//> using dep "org.virtuslab::besom-core:0.2.3-SNAPSHOT"
+//> using dep "org.virtuslab::besom-azure-native:2.29.0-core.0.2"
+
+//> using repository sonatype:snapshots
