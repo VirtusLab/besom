e99094f7ddcda1d02fcccbf4624c6cc209862af3
Paweł Prażak <pprazak@virtuslab.com>
2024-04-11 15:54:31 +0200

    Update components example
    - update kubernetes guestbook components

--- examples/kubernetes-guestbook/components/Main.scala
+++ examples/kubernetes-guestbook/components/Main.scala
@@ -1,130 +1,336 @@
 import besom.*
 import besom.api.kubernetes
-import besom.api.kubernetes.core.v1.{Service, ServiceArgs}
-import besom.api.kubernetes.core.v1.inputs.*
-import besom.api.kubernetes.apps.v1.{Deployment, DeploymentArgs}
-import besom.api.kubernetes.apps.v1.inputs.*
+import besom.api.kubernetes.apps.v1.inputs.DeploymentSpecArgs
+import besom.api.kubernetes.apps.v1.*
+import besom.api.kubernetes.core
+import besom.api.kubernetes.core.v1.*
+import besom.api.kubernetes.core.v1.enums.*
+import besom.api.kubernetes.core.v1.inputs.{
+  ContainerArgs,
+  ContainerPortArgs,
+  EnvVarArgs,
+  PodSpecArgs,
+  PodTemplateSpecArgs,
+  ResourceRequirementsArgs,
+  ServicePortArgs,
+  ServiceSpecArgs,
+  VolumeArgs,
+  VolumeMountArgs
+}
 import besom.api.kubernetes.meta.v1.*
 import besom.api.kubernetes.meta.v1.inputs.*
-import besom.api.kubernetes.core.v1.enums.ServiceSpecType
 
-case class ServiceDeploymentArgs(
-  image: String,
-  replicas: Int = 1,
-  resources: Option[ResourceRequirementsArgs] = None,
-  ports: List[Int] = List.empty,
-  allocateIPAddress: Option[Boolean] = None,
-  serviceType: Option[ServiceSpecType] = None,
-  env: List[EnvVarArgs] = List.empty
+case class RedisEndpoint(url: Output[String], fqdn: Output[String]) derives Encoder
+object RedisEndpoint:
+  extension (r: Output[RedisEndpoint])
+    def url: Output[String]  = r.flatMap(_.url)
+    def fqdn: Output[String] = r.flatMap(_.fqdn)
+case class Redis private (
+  namespace: Output[String],
+  leader: Output[RedisEndpoint],
+  replica: Output[RedisEndpoint]
+)(using ComponentBase)
+    extends ComponentResource
+    derives RegistersOutputs
+object Redis:
+  extension (r: Output[Redis])
+    def namespace: Output[String]      = r.flatMap(_.namespace)
+    def leader: Output[RedisEndpoint]  = r.flatMap(_.leader)
+    def replica: Output[RedisEndpoint] = r.flatMap(_.replica)
+
+  def apply(using Context)(
+    name: NonEmptyString,
+    replicas: Input[Int] = 1,
+    options: ComponentResourceOptions = ComponentResourceOptions()
+  ): Output[Redis] =
+    component(name, "besom:example:Redis", options) {
+      val namespace                  = Namespace(s"redis-$name")
+      val leaderName: NonEmptyString = "redis-leader"
+      val leaderLabels               = Map("app" -> leaderName)
+
+      val redisPortName   = "client"
+      val redisPortNumber = 6379
+
+      val leader = Deployment(
+        leaderName,
+        DeploymentArgs(
+          metadata = ObjectMetaArgs(
+            name = leaderName,
+            namespace = namespace.metadata.name,
+            labels = leaderLabels
+          ),
+          spec = DeploymentSpecArgs(
+            selector = LabelSelectorArgs(
+              matchLabels = leaderLabels
+            ),
+            template = PodTemplateSpecArgs(
+              metadata = ObjectMetaArgs(
+                name = leaderName,
+                namespace = namespace.metadata.name,
+                labels = leaderLabels
+              ),
+              spec = PodSpecArgs(
+                containers = List(
+                  ContainerArgs(
+                    name = "redis",
+                    image = "redis",
+                    ports = List(
+                      ContainerPortArgs(name = redisPortName, containerPort = redisPortNumber)
+                    ),
+                    resources = ResourceRequirementsArgs(
+                      requests = Map(
+                        "cpu" -> "100m",
+                        "memory" -> "100Mi"
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+
+      val leaderService = Service(
+        leaderName,
+        ServiceArgs(
+          metadata = ObjectMetaArgs(
+            name = leaderName,
+            labels = leaderLabels,
+            namespace = namespace.metadata.name
+          ),
+          spec = ServiceSpecArgs(
+            ports = List(
+              ServicePortArgs(
+                name = redisPortName,
+                port = redisPortNumber,
+                targetPort = redisPortNumber
+              )
+            ),
+            selector = leader.spec.template.metadata.labels
+          )
+        )
+      )
+
+      val replicaName: NonEmptyString = "redis-replica"
+      val replicaLabels               = Map("app" -> replicaName)
+
+      val replica = Deployment(
+        replicaName,
+        DeploymentArgs(
+          metadata = ObjectMetaArgs(
+            name = replicaName,
+            namespace = namespace.metadata.name,
+            labels = replicaLabels
+          ),
+          spec = DeploymentSpecArgs(
+            selector = LabelSelectorArgs(
+              matchLabels = replicaLabels
+            ),
+            replicas = replicas,
+            template = PodTemplateSpecArgs(
+              metadata = ObjectMetaArgs(
+                name = replicaName,
+                namespace = namespace.metadata.name,
+                labels = replicaLabels
+              ),
+              spec = PodSpecArgs(
+                containers = List(
+                  ContainerArgs(
+                    name = "redis",
+                    image = "pulumi/guestbook-redis-replica",
+                    env = List(
+                      EnvVarArgs(name = "GET_HOSTS_FROM", value = "dns")
+                    ),
+                    ports = List(
+                      ContainerPortArgs(name = redisPortName, containerPort = redisPortNumber)
+                    ),
+                    resources = ResourceRequirementsArgs(
+                      requests = Map(
+                        "cpu" -> "100m",
+                        "memory" -> "100Mi"
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+
+      val replicaService = Service(
+        replicaName,
+        ServiceArgs(
+          metadata = ObjectMetaArgs(
+            name = replicaName,
+            labels = replicaLabels,
+            namespace = namespace.metadata.name
+          ),
+          spec = ServiceSpecArgs(
+            ports = List(
+              ServicePortArgs(
+                name = redisPortName,
+                port = redisPortNumber,
+                targetPort = redisPortNumber
+              )
+            ),
+            selector = replica.spec.template.metadata.labels
+          )
+        )
+      )
+
+      new Redis(
+        namespace = namespace.metadata.name.getOrFail(Exception("expected namespace name to be defined")),
+        leader = Output(
+          RedisEndpoint(
+            url = p"redis://${serviceFqdn(leaderService, namespace)}:$redisPortNumber",
+            fqdn = serviceFqdn(leaderService, namespace)
+          )
+        ),
+        replica = Output(
+          RedisEndpoint(
+            url = p"redis://${serviceFqdn(replicaService, namespace)}:$redisPortNumber",
+            fqdn = serviceFqdn(replicaService, namespace)
+          )
+        )
+      )
+    }
+end Redis
+
+case class ApplicationArgs(
+  redis: Input[Redis],
+  serviceType: Input[ServiceSpecType],
+  image: Input[String] = "pulumi/guestbook-php-redis",
+  replicas: Input[Int] = 1,
+  resources: Input[Option[ResourceRequirementsArgs]] = None
 )
 
-case class ServiceDeployment(ipAddress: Output[Option[String]])(using ComponentBase) extends ComponentResource derives RegistersOutputs
+case class Application private (
+  namespace: Output[String],
+  url: Output[String],
+  fqdn: Output[String]
+)(using ComponentBase)
+    extends ComponentResource
+    derives RegistersOutputs
+object Application:
+  extension (r: Output[Application])
+    def namespace: Output[String] = r.flatMap(_.namespace)
+    def url: Output[String]       = r.flatMap(_.url)
+    def fqdn: Output[String]      = r.flatMap(_.fqdn)
 
-def serviceDeployment(using Context)(
-  name: NonEmptyString,
-  args: ServiceDeploymentArgs,
-  componentResourceOptions: ComponentResourceOptions = ComponentResourceOptions()
-): Output[ServiceDeployment] =
-  component(name, "k8sx:service:ServiceDeployment", componentResourceOptions) {
-    val labels = Map("app" -> name)
-    val deploymentPorts = args.ports
-      .map(port => ContainerPortArgs(containerPort = port))
-
-    val container = ContainerArgs(
-      name = name,
-      image = args.image,
-      resources = args.resources.getOrElse(
-        ResourceRequirementsArgs(
-          requests = Map(
-            "cpu" -> "100m",
-            "memory" -> "100Mi"
-          )
-        )
-      ),
-      env = args.env,
-      ports = deploymentPorts
-    )
-
-    val deployment = Deployment(
-      name,
-      DeploymentArgs(
-        spec = DeploymentSpecArgs(
-          selector = LabelSelectorArgs(matchLabels = labels),
-          replicas = args.replicas,
-          template = PodTemplateSpecArgs(
-            metadata = ObjectMetaArgs(labels = labels),
-            spec = PodSpecArgs(containers = List(container))
+  def apply(using Context)(
+    name: NonEmptyString,
+    args: ApplicationArgs,
+    componentResourceOptions: ComponentResourceOptions = ComponentResourceOptions()
+  ): Output[Application] =
+    component(name, "besom:example:Application", componentResourceOptions) {
+      val labels = Map("app" -> name)
+      val namespace = Namespace(
+        name,
+        NamespaceArgs(
+          metadata = ObjectMetaArgs(
+            labels = labels
           )
         )
       )
-    )
 
-    val servicePorts = args.ports
-      .map(port => ServicePortArgs(port = port, targetPort = port))
-
-    val service = Service(
-      name,
-      ServiceArgs(
-        metadata = ObjectMetaArgs(labels = labels, name = name),
-        spec = ServiceSpecArgs(
-          `type` = args.allocateIPAddress
-            .flatMap(Option.when(_)(args.serviceType.getOrElse(ServiceSpecType.LoadBalancer))),
-          ports = servicePorts,
-          selector = labels
+      val appPortNumber = 80
+      val deployment = Deployment(
+        name,
+        DeploymentArgs(
+          metadata = ObjectMetaArgs(
+            labels = labels,
+            namespace = namespace.metadata.name
+          ),
+          spec = DeploymentSpecArgs(
+            selector = LabelSelectorArgs(matchLabels = labels),
+            replicas = args.replicas,
+            template = PodTemplateSpecArgs(
+              metadata = ObjectMetaArgs(labels = labels),
+              spec = PodSpecArgs(
+                containers = List(
+                  ContainerArgs(
+                    name = name,
+                    image = args.image,
+                    ports = List(
+                      ContainerPortArgs(name = "http", containerPort = appPortNumber)
+                    ),
+                    env = List(
+                      EnvVarArgs(name = "GET_HOSTS_FROM", value = "env"),
+                      EnvVarArgs(name = "REDIS_LEADER_SERVICE_HOST", value = args.redis.asOutput().leader.fqdn),
+                      EnvVarArgs(name = "REDIS_REPLICA_SERVICE_HOST", value = args.redis.asOutput().replica.fqdn)
+                    ),
+                    resources = args.resources
+                      .asOptionOutput()
+                      .getOrElse(
+                        ResourceRequirementsArgs(
+                          requests = Map(
+                            "cpu" -> "100m",
+                            "memory" -> "100Mi"
+                          )
+                        )
+                      )
+                  )
+                )
+              )
+            )
+          )
         )
       )
-    )
 
-    val ipAddress =
-      for
-        hasIp       <- args.allocateIPAddress
-        serviceType <- args.serviceType if hasIp
-      yield
-        if serviceType == ServiceSpecType.ClusterIP
-        then service.spec.clusterIP
-        else service.status.loadBalancer.ingress.map(_.map(_.head)).ip
+      val service = Service(
+        name,
+        ServiceArgs(
+          metadata = ObjectMetaArgs(
+            labels = labels,
+            namespace = namespace.metadata.name
+          ),
+          spec = ServiceSpecArgs(
+            selector = deployment.spec.template.metadata.labels,
+            `type` = args.serviceType,
+            ports = List(
+              ServicePortArgs(name = "http", port = appPortNumber, targetPort = appPortNumber)
+            )
+          )
+        )
+      )
 
-    for
-      _ <- deployment
-      _ <- service
-    yield ServiceDeployment(ipAddress.getOrElse(Output(None)))
-  }
+      val fqdn         = serviceFqdn(service, namespace)
+      val maybeIngress = service.status.loadBalancer.ingress.headOption
+      val hostnameOrIp = maybeIngress.hostname.orElse(maybeIngress.ip)
+      val url          = p"http://${hostnameOrIp.getOrElse(fqdn)}:$appPortNumber"
 
-@main def main = Pulumi.run {
-  val redisLeader = serviceDeployment(
-    name = "redis-leader",
-    args = ServiceDeploymentArgs(
-      image = "redis",
-      ports = List(6379)
+      new Application(
+        namespace = namespace.metadata.name.getOrFail(Exception("expected namespace name to be defined")),
+        url = url,
+        fqdn = fqdn
+      )
+    }
+end Application
+
+private def serviceFqdn(service: Output[Service], namespace: Output[Namespace])(using Context): Output[String] =
+  val serviceName   = service.metadata.name.getOrFail(Exception("expected service name to be defined"))
+  val namespaceName = namespace.metadata.name.getOrFail(Exception("expected namespace name to be defined"))
+  p"${serviceName}.${namespaceName}.svc.cluster.local"
+
+@main def main(): Unit = Pulumi.run {
+  val useLoadBalancer = config.getBoolean("useLoadBalancer").getOrElse(true)
+
+  val redis = Redis("cache", 3)
+
+  val frontend = Application(
+    name = "frontend",
+    args = ApplicationArgs(
+      replicas = 3,
+      serviceType = useLoadBalancer.map(if _ then ServiceSpecType.LoadBalancer else ServiceSpecType.ClusterIP),
+      redis = redis
     )
   )
 
-  val redisReplica = serviceDeployment(
-    name = "redis-replica",
-    args = ServiceDeploymentArgs(
-      image = "pulumi/guestbook-redis-replica",
-      ports = List(6379)
-    )
+  Stack.exports(
+    frontend = frontend.url,
+    leader = redis.leader,
+    replica = redis.replica
   )
-
-  val frontend = config
-    .getBoolean("isMinikube")
-    .getOrElse(false)
-    .flatMap(isMinikube =>
-      serviceDeployment(
-        name = "frontend",
-        args = ServiceDeploymentArgs(
-          replicas = 3,
-          image = "pulumi/guestbook-php-redis",
-          ports = List(80),
-          allocateIPAddress = Some(true),
-          serviceType = Some(if isMinikube then ServiceSpecType.ClusterIP else ServiceSpecType.LoadBalancer)
-        )
-      )
-    )
-
-  Stack(redisLeader, redisReplica)
-    .exports(
-      ipAddress = frontend.flatMap(_.ipAddress)
-    )
 }
--- examples/kubernetes-guestbook/components/README.md
+++ examples/kubernetes-guestbook/components/README.md
@@ -16,44 +16,58 @@ Enter a stack name: testbook
 
 This example will attempt to expose the Guestbook application to the Internet with a `Service` of
 type `LoadBalancer`. Since minikube does not support `LoadBalancer`, the Guestbook application
-already knows to use type `ClusterIP` instead; all you need to do is to tell it whether you're
-deploying to minikube:
+already knows to use type `ClusterIP` instead. All you need to do is to tell it whether you're
+deploying to use the load balancer or not. 
+
+You can do this by setting the `useLoadBalancer` configuration value to (default is `true`):
 
 ```sh
-pulumi config set isMinikube <value>
+pulumi config set useLoadBalancer <value>
 ```
 
 Perform the deployment:
 
 ```sh
 $ pulumi up
+...
 Updating stack 'testbook'
 Performing changes:
 
-     Type                           Name                       Status
- +   pulumi:pulumi:Stack            guestbook-easy-testbook  created
- +   ├─ kubernetes:apps:Deployment  redis-replica              created
- +   ├─ kubernetes:apps:Deployment  frontend                   created
- +   ├─ kubernetes:apps:Deployment  redis-leader               created
- +   ├─ kubernetes:core:Service     redis-leader               created
- +   ├─ kubernetes:core:Service     redis-replica              created
- +   └─ kubernetes:core:Service     frontend                   created
+     Type                                 Name                Status
+ +   pulumi:pulumi:Stack                  guestbook-easy-dev  created (54s)
+ +   ├─ besom:example:Application         frontend            created (20s)
+ +   │  ├─ kubernetes:core/v1:Namespace   frontend            created (0.33s)
+ +   │  ├─ kubernetes:apps/v1:Deployment  frontend            created (5s)
+ +   │  └─ kubernetes:core/v1:Service     frontend            created (10s)
+ +   └─ besom:example:Redis               cache               created (21s)
+ +      ├─ kubernetes:core/v1:Namespace   redis-cache         created (0.26s)
+ +      ├─ kubernetes:apps/v1:Deployment  redis-leader        created (7s)
+ +      ├─ kubernetes:core/v1:Service     redis-leader        created (10s)
+ +      ├─ kubernetes:apps/v1:Deployment  redis-replica       created (9s)
+ +      └─ kubernetes:core/v1:Service     redis-replica       created (10s)
 
 Outputs:
-  + FrontendIp: "35.232.147.18"
+    frontend: "http://localhost:80"
+    leader  : {
+        fqdn: "redis-leader.redis-cache-623162a5.svc.cluster.local"
+        url : "redis://redis-leader.redis-cache-623162a5.svc.cluster.local:6379"
+    }
+    replica : {
+        fqdn: "redis-replica.redis-cache-623162a5.svc.cluster.local"
+        url : "redis://redis-replica.redis-cache-623162a5.svc.cluster.local:6379"
+    }
 
 Resources:
-    + 7 created
+    + 11 created
 
-Duration: 17s
-
+Duration: 58s
 ```
 
 And finally - open the application in your browser to see the running application. If you're running
 macOS you can simply run:
 
 ```sh
-open $(pulumi stack output FrontendIp)
+open $(pulumi stack output frontend)
 ```
 
 > _Note_: minikube does not support type `LoadBalancer`; if you are deploying to minikube, make sure
