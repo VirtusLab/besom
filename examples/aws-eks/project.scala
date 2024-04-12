e84bc50dfb4622966061eaa91a0ce04a7117778a
Paweł Prażak <pprazak@virtuslab.com>
2024-04-12 11:23:18 +0200

    Remove compiler plugin from examples

--- examples/aws-eks/project.scala
+++ examples/aws-eks/project.scala
@@ -1,6 +1,5 @@
 //> using scala "3.3.1"
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
-//> using plugin "org.virtuslab::besom-compiler-plugin:0.3.0-SNAPSHOT"
 
 //> using dep "org.virtuslab::besom-core:0.3.0-SNAPSHOT"
 //> using dep "org.virtuslab::besom-awsx:2.6.0-core.0.3-SNAPSHOT"
--- experimental/project.scala
+++ experimental/project.scala
@@ -1,5 +1,4 @@
 //> using scala "3.3.1"
-//> using plugin "org.virtuslab::besom-compiler-plugin:0.3.0-SNAPSHOT"
 //> using dep "org.virtuslab::besom-core:0.3.0-SNAPSHOT"
 //> using dep "org.virtuslab::besom-kubernetes:4.9.1-core.0.3-SNAPSHOT"
 //> using dep "io.github.iltotore::iron:2.5.0"
