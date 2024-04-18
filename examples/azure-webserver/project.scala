f9e71081bf1ec6d1c0d151460085ca6a4c8335bd
kpoliwka <kpoliwka@virtuslab.com>
2024-04-18 09:50:19 +0200

    Bump version to 0.3.0

--- examples/azure-webserver/project.scala
+++ examples/azure-webserver/project.scala
@@ -1,8 +1,4 @@
 //> using scala "3.3.1"
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
-//> using plugin "org.virtuslab::besom-compiler-plugin:0.3.0-SNAPSHOT"
-
-//> using dep "org.virtuslab::besom-core:0.3.0-SNAPSHOT"
-//> using dep "org.virtuslab::besom-azure-native:2.36.0-core.0.3-SNAPSHOT"
-
-//> using repository sonatype:snapshots
+//> using dep "org.virtuslab::besom-core:0.3.0"
+//> using dep "org.virtuslab::besom-azure-native:2.37.0-core.0.3"
