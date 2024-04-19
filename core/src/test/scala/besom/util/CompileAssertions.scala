7811f3b982650d9e4cb2394ebb62f3862389e97a
Łukasz Biały <lbialy@virtuslab.com>
2024-04-19 12:37:01 +0200

    fix issue 462 - pulumi string interpolation inference macro threw MatchError on dynamic Output[String] (#465)

--- core/src/test/scala/besom/util/CompileAssertions.scala
+++ core/src/test/scala/besom/util/CompileAssertions.scala
@@ -10,13 +10,13 @@ trait CompileAssertions:
 
   private val NL = System.lineSeparator()
 
-  inline def failsToCompile(inline code: String): Unit =
+  transparent inline def failsToCompile(inline code: String): Unit =
     assert(
       !scala.compiletime.testing.typeChecks(code),
       s"Code compiled correctly when expecting type errors:$NL$code"
     )
 
-  inline def compiles(inline code: String): Unit =
+  transparent inline def compiles(inline code: String): Unit =
     val errors = scala.compiletime.testing.typeCheckErrors(code)
     if errors.nonEmpty then
       val errorMessages = errors.map(_.message).mkString(NL)
