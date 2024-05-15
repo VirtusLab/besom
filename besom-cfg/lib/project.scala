296a64e613edc5c4d9e27d13e08d12e59f8fba11
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-05-15 17:12:13 +0200

    besom-cfg initial checkin to monorepo

---
+++ besom-cfg/lib/project.scala
@@ -0,0 +1,18 @@
+//> using scala 3.3.3
+
+//> using dep org.virtuslab::besom-core::0.4.0-SNAPSHOT
+//> using dep org.virtuslab::besom-json::0.4.0-SNAPSHOT
+//> using dep io.github.classgraph:classgraph:4.8.172
+
+//> using test.dep org.scalameta::munit::1.0.0-M11
+
+//> using publish.name "besom-cfg"
+//> using publish.organization "org.virtuslab"
+//> using publish.url "https://github.com/VirtusLab/besom"
+//> using publish.vcs "github:VirtusLab/besom"
+//> using publish.license "Apache-2.0"
+//> using publish.repository "central"
+//> using publish.developer "lbialy|Łukasz Biały|https://github.com/lbialy"
+//> using publish.developer "prolativ|Michał Pałka|https://github.com/prolativ"
+//> using publish.developer "KacperFKorban|Kacper Korban|https://github.com/KacperFKorban"
+//> using publish.developer "pawelprazak|Paweł Prażak|https://github.com/pawelprazak"
