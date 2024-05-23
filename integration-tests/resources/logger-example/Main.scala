ea990a7ad1bc44452c55f60e0e1de2bbb9d16b90
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-05-23 16:53:48 +0200

    fix integration test

--- integration-tests/resources/logger-example/Main.scala
+++ integration-tests/resources/logger-example/Main.scala
@@ -3,7 +3,6 @@ import besom.*
 @main def run = Pulumi.run {
   Stack(
     log.warn("Nothing here yet. It's waiting for you!"),
-    p"Interpolated ${Output("value")}".flatMap(log.info(_)),
-    log.trace("should not show up in pulumi output")
+    p"Interpolated ${Output("value")}".flatMap(log.info(_))
   )
 }
