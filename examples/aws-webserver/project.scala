f1fb6f384cbf7af3162311bbee894325982fbc49
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-06-10 14:25:16 +0200

    fix versions

--- examples/aws-webserver/project.scala
+++ examples/aws-webserver/project.scala
@@ -2,7 +2,7 @@
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
 //> using plugin org.virtuslab::besom-compiler-plugin:0.3.2-SNAPSHOT
 //> using dep org.virtuslab::besom-core:0.3.2-SNAPSHOT
-//> using dep org.virtuslab::besom-aws:6.39.0-core.0.3-SNAPSHOT
-//> using dep org.virtuslab::besom-tls:5.0.3-core.0.3-SNAPSHOT
+//> using dep org.virtuslab::besom-aws:6.39.0-core.0.3
+//> using dep org.virtuslab::besom-tls:5.0.3-core.0.3
 
 //> using repository sonatype:snapshots
--- templates/default/project.scala
+++ templates/default/project.scala
@@ -1,6 +1,6 @@
 //> using scala "3.3.1"
 //> using options -Werror -Wunused:all -Wvalue-discard -Wnonunit-statement
 //> using dep "org.virtuslab::besom-core:0.3.2-SNAPSHOT"
-//> using dep "org.virtuslab::besom-random:4.16.2-core.0.3-SNAPSHOT"
+//> using dep "org.virtuslab::besom-random:4.16.2-core.0.3"
 
 //> using repository sonatype:snapshots
