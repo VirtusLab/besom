c3ae30334b6ee70eb4eb7439b8d3887d2554a84c
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-09-14 15:24:40 +0200

    fix besom-cfg-k8s tests

--- besom-cfg/k8s/src/test/scala/RunOutput.scala
+++ besom-cfg/k8s/src/test/scala/RunOutput.scala
@@ -3,11 +3,11 @@ package besom.internal
 import scala.concurrent.Future
 
 trait RunOutput[F[+_]]:
-  def run[A](output: Output[A]): Option[A]
+  def run[A](output: Output[A])(using Context): Option[A]
 
 object RunOutput:
   given runOutputForFuture(using RunResult[Future]): RunOutput[Future] = new RunOutput[Future]:
-    def run[A](output: Output[A]): Option[A] = RunResult.unsafeRunSync(output.getData)().getValue
+    def run[A](output: Output[A])(using Context): Option[A] = RunResult.unsafeRunSync(output.getData)().getValue
 
-  extension [F[+_], A](output: Output[A])(using ro: RunOutput[F]) def unsafeRunSync(): Option[A] = ro.run(output)
-  extension [F[+_], A](result: Result[A])(using rr: RunResult[F]) def unsafeRunSync(): A         = rr.run(result)
+  extension [F[+_], A](output: Output[A])(using ro: RunOutput[F]) def unsafeRunSync()(using Context): Option[A] = ro.run(output)
+  extension [F[+_], A](result: Result[A])(using rr: RunResult[F]) def unsafeRunSync(): A                        = rr.run(result)
