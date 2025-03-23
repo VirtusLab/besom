1d836df6c6b5b95069796b06741616e4fc42828d
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2025-03-23 16:18:06 +0100

    Bumped versions and links to 0.4

--- scripts/project.scala
+++ scripts/project.scala
@@ -10,7 +10,7 @@
 //> using dep org.scoverage::scalac-scoverage-serializer:2.0.11
 //> using dep org.virtuslab::scala-yaml:0.3.0
 //> using dep io.get-coursier:coursier_2.13:2.1.9
-//> using dep org.virtuslab::besom-codegen:0.4.0-SNAPSHOT
+//> using dep org.virtuslab::besom-codegen:0.4.0
 //> using test.dep org.scalameta::munit::1.0.0-M10
 
 //> using publish.name "besom-scripts"
