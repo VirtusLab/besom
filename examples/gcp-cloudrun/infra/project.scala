51a7ae181e01838a4664fcbb822ea9117505e285
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2025-06-16 12:30:58 +0200

    update examples 2

--- examples/gcp-cloudrun/infra/project.scala
+++ examples/gcp-cloudrun/infra/project.scala
@@ -3,8 +3,8 @@
 ///> using plugin "org.virtuslab::besom-compiler-plugin:0.4.0"
 
 //> using dep "org.virtuslab::besom-core:0.5.0-SNAPSHOT"
-//> using dep "org.virtuslab::besom-gcp:8.22.0-core.0.4"
-//> using dep "org.virtuslab::besom-docker:4.6.2-core.0.4"
+//> using dep "org.virtuslab::besom-gcp:8.22.0-core.0.5-SNAPSHOT"
+//> using dep "org.virtuslab::besom-docker:4.6.2-core.0.5-SNAPSHOT"
 
 //> using exclude "app/*"
 //> using repository sonatype:snapshots
