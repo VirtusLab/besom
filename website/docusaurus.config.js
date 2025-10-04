f1187ca1dee71fa1c67d12fc4e57b04a2bbdd323
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2025-10-04 15:10:54 +0200

    fix warnings from docusaurus blog plugin

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -40,7 +40,11 @@ const config = {
   baseUrl: `/${projectName}/`,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'throw'
+    }
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
