e0575ccd2c6f92d266494c3f11d0604a35a98459
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2026-09-15 19:28:31 +0200

    bump to 0.5.2

--- language-plugin/bootstrap/project.scala
+++ language-plugin/bootstrap/project.scala
@@ -1,6 +1,5 @@
 //> using scala 3.3.7
 //> using options -java-output-version:11
 
-//> using dep org.virtuslab::besom-json:0.5.2-SNAPSHOT
+//> using dep org.virtuslab::besom-json:0.5.2
 //> using dep io.github.classgraph:classgraph:4.8.180
-//> using repository snapshots
